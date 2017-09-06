b55495d648d1df8695002a7b7138bceb19f787a6
EdwardMudaida <eddieigo8@gmail.com>
2017-09-06 18:31:05 +0300

    Scaffold circle

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906092420) do
+ActiveRecord::Schema.define(version: 20170906152800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20170906092420) do
     t.string "school"
     t.text "aspirations"
     t.string "siblings"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "circles", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
