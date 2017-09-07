5690d8af4c80a559ee61c18cb4b23a126e0d719c
Nauphrage <nauphragecompilers@gmail.com>
2017-09-07 12:14:18 +0300

    added errors partial added CRUD functionality for child controller added form for adding new child

--- app/controllers/child_controller.rb
+++ app/controllers/child_controller.rb
@@ -13,5 +13,33 @@ class ChildrenController < ApplicationController
       @child = Child.new
       render :new
     end
-    
+
+    def create
+      @child = Child.new(child_params)
+    if @child.save
+      redirect_to children_path
+    else
+      render:new
+    end
+  end
+
+  def edit
+    @child = Child.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @child = Child.find(params[:id])
+    if @child.update(child_params)
+      redirect_to children_path
+    else
+      render :edit
+    end
+  end
+  
+  private
+  def child_params
+    params.require(:child).permit(:name, :age, :home, :school, :aspirations, :siblings)
+  end
+
   end
