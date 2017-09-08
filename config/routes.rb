290989b3195d5a1c29b63521074c563c79f833cb
anthonyshikanga <anthonyshikanga@gmail.com>
2017-09-08 14:01:40 +0300

    added is_admin boolean to user model and modified routes.rb with admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :circles
   devise_for :users
   get 'home/index'
+  authenticated :user do
+  	root 'users#dashboard', as: "authenticated_root" 
+  end
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.create(
+	email: 'admin@gmail.com',
+	password: 'admin',
+	is_admin: true
+
+	)
