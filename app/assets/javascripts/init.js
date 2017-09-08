3f5369126cf5feda2cda6e4011027253354c76d3
EdwardMudaida <eddieigo8@gmail.com>
2017-09-08 11:07:27 +0300

    Adds custom styling

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,15 +1,13 @@
-$(document).ready(function(){
-   var scroll_start = 0;
-   var startchange = $('#startchange');
-   var offset = startchange.offset();
-   $(document).scroll(function() {
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $('#navbar').css('background-color', 'white');
-          $('.nav-li').css('color', 'black');
-       } else {
-          $('#navbar').css('background-color', 'transparent');
-          $('.nav-li').css('color', 'white');
-       }
-   });
-});
+// $(document).ready(function(){
+//    var scroll_start = 0;
+//    var startchange = $('#startchange');
+//    var offset = startchange.offset();
+//    $(document).scroll(function() {
+//       scroll_start = $(this).scrollTop();
+//       if(scroll_start > offset.top) {
+//           $('#navbar').css('background-color', '#f4f3f2');
+//        } else {
+//           $('#navbar').css('background-color', 'transparent');
+//        }
+//    });
+// });
