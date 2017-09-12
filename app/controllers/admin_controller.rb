632d1260bd447eb1685a043679dd2acc3fcef36e
anthonyshikanga <anthonyshikanga@gmail.com>
2017-09-12 11:14:45 +0300

    modified files

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,14 +8,5 @@ class AdminController < ApplicationController
 			redirect_to root_path, alert: "You must be an admin to access this path."
 		end
 	end
+
 end
-
-	def check_for_admin
-		if current_user.nil? || !current_user.is_admin?
-			redirect_to root_path, alert: "You must be admin to access this path"
-		end
-
-	end
-end
-
-
