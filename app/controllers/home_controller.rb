1b3d499fab9617ea6d72f436f7cc485b0ed1d73c
EdwardMudaida <eddieigo8@gmail.com>
2017-09-15 15:22:07 +0300

    Makes current users details available acros the app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def index
     @children = Child.all
-    @user = current_user.id
+    if current_user
+      @user = current_user.id
+      # @circles = User.joins(:memberships).where(memberships:{user_id: @user.id})
+    end
     render :index
   end
 end
