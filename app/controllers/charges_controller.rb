6c89d444b4351eddf6d1c9f65b532bbd85d3bc66
anthonyshikanga <anthonyshikanga@gmail.com>
2017-09-19 15:02:58 +0300

    fixed merge conflicts

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,28 @@
+
 class ChargesController < ApplicationController
-def new
+	def new
     end
 
  def create
   # Amount in cents
-  @amount = 500
+  @amount = 20000
+
 
   customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
-    :source  => params[:stripeToken]
+    # :source  => params[:stripeToken]
   )
 
   charge = Stripe::Charge.create(
     :customer    => customer.id,
     :amount      => @amount,
     :description => 'Rails Stripe customer',
-    :currency    => 'usd'
+    :currency    => 'KES'
   )
 
 rescue Stripe::CardError => e
   flash[:error] = e.message
-  redirect_to new_charge_path
+  redirect_to circles_path
 end
 end
+
