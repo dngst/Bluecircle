bea68debf77927a8c423a17a7b01b607007c48a9
EdwardMudaida <eddieigo8@gmail.com>
2017-09-19 18:06:45 +0300

    Adds config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,27 @@ Rails.application.routes.draw do
   end
 
   resources :circles  do
-    get 'join', on: :member
+  	get 'join', on: :member
   end
-  
+
   devise_for :users
   get 'users/:id' => 'users#show', as: :profile
   resources :children
   get 'home/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :circles, only: [:index, :show]
-  devise_for :users
 
-  authenticated :user do
-    root 'circles#index', as: "authenticated_root"
+  resources :charges, only: [:new, :create]
+    authenticated :user do
+  	root 'circles#index', as: "authenticated_root"
+
   end
+
+  root 'home#index'
+  # namespace :admin do
+  # 	resources :circles
+  # 	resources :children, only:[:new, :edit, :show,:index]
+  # end
+  resources :children, only:[:index, :show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
