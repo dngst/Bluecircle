ef90d97f094b2df0703fc1c3aa00b6c1e9f67a5c
Nauphrage <nauphragecompilers@gmail.com>
2017-09-19 13:58:57 +0300

    added link to blog on nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915091144) do
+ActiveRecord::Schema.define(version: 20170918124525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.text "title"
+    t.text "body"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "children", force: :cascade do |t|
     t.string "name"
