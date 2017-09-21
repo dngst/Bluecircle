02b2f57e28521ebf3fda8569734e5b81f8574c01
EdwardMudaida <eddieigo8@gmail.com>
2017-09-21 15:56:57 +0300

    Adds blog seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,42 @@ Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/
 Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
 Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+
+Blog.create(title: "Blog Title Goes Here", body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum", image_url: "https://static.pexels.com/photos/129105/pexels-photo-129105.jpeg")
+
+Blog.create(title: "Blog Title Goes Here", body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum", image_url: "https://static.pexels.com/photos/129105/pexels-photo-129105.jpeg")
+
+Blog.create(title: "Blog Title Goes Here", body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum", image_url: "https://static.pexels.com/photos/129105/pexels-photo-129105.jpeg")
+Blog.create(title: "Blog Title Goes Here", body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum", image_url: "https://static.pexels.com/photos/129105/pexels-photo-129105.jpeg")
+Blog.create(title: "Blog Title Goes Here", body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum", image_url: "https://static.pexels.com/photos/129105/pexels-photo-129105.jpeg")
+Blog.create(title: "Blog Title Goes Here", body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum", image_url: "https://static.pexels.com/photos/129105/pexels-photo-129105.jpeg")
