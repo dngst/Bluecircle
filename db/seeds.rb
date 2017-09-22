17a6f3cef5aa7e3864e9fc52c56e21d24f64b267
anthonyshikanga <anthonyshikanga@gmail.com>
2017-09-22 10:04:45 +0300

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Alex Waweru", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Alex loves sharks.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"James Muthee", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Faith Njoki", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Jerome Onyango", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Wesley Okumu", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Anita Kemboi", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Dennis Shitanda", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Brian Mukumu", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Lisa Anyika", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Davies Wahome", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Charles Luanga", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Kwese Obura", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Child Name", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Alvin Moses", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
