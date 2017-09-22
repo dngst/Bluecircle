cd493463b96801f995b59b6a3f02155fc0a4d4f1
anthonyshikanga <anthonyshikanga@gmail.com>
2017-09-22 10:44:40 +0300

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,28 @@
 
-Circle.create(name:"Alex Waweru", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Alex loves sharks.")
+Circle.create(name:"Alex Waweru", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToB4SDieIgPxIwZEr5CRnQwct5Fj_Dx_7nN0SDPnc--1Uxvlwo", description:"Alex loves sharks.")
 
-Circle.create(name:"James Muthee", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"James Muthee", image:"https://s1.favim.com/610/150929/african-child-cute-greek-quotes-Favim.com-3375620.jpg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Faith Njoki", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Faith Njoki", image:"https://upload.wikimedia.org/wikipedia/commons/8/8b/AFRICAN_CHILD.jpg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Jerome Onyango", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Jerome Onyango", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPa_r16bs2h7YqZqjuOItTM8tPQz56O7960mwMLX3t6cFb9I6MIA", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Wesley Okumu", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Wesley Okumu", image:"https://thechangewewant.files.wordpress.com/2014/10/african-child.jpg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Anita Kemboi", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Anita Kemboi", image:"https://answersafrica.com/wp-content/uploads/2016/10/Girl-Child-Day-1024x683.jpg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Dennis Shitanda", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Dennis Shitanda", image:"https://jillsbooks.files.wordpress.com/2010/08/african-child-2_225x300.jpg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Brian Mukumu", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Brian Mukumu", image:"http://www.pedaids.org/page/-/uploads/blog-content-images/_cropped-800/_DSC8205-2_2.jpg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Lisa Anyika", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Lisa Anyika", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST8MRUYA1MiIHmqV_96bNwyY2aCpO7xiu64l5OLHKQDTgyYgmDcw", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Davies Wahome", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Davies Wahome", image:"https://www.childrenincrossfire.org/wp-content/uploads/2017/06/try-300x245.jpg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Charles Luanga", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Charles Luanga", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs65WbHSBz01gQR61MCiTgFqYB2orrBt9QyP0ME6gNWxs79xgS", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Kwese Obura", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Kwese Obura", image:"https://blog.compassion.com/wp-content/uploads/2015/08/day-of-the-african-child-glasses-out-of-wire.jpg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
-Circle.create(name:"Alvin Moses", image:"https://static.pexels.com/photos/259269/pexels-photo-259269.jpeg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
+Circle.create(name:"Alvin Moses", image:"https://heleenvandeven.files.wordpress.com/2013/04/img_1219-cute-african-child.jpg", description:"Some quick example text to build on the card title and make up the bulk of the card's content.")
 
 
